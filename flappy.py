2a931101ab2dad83b39ad9a07d7a2924730b5950
SchoolNerdProgrammer <colemarshall12389@gmail.com>
2023-03-24 15:06:55 +0000

    From PC update- fixed error

--- flappy.py
+++ flappy.py
@@ -75,6 +75,12 @@ class Alien(pygame.sprite.Sprite):
         else:
             self.image = pygame.transform.rotate(self.images[self.index], 90)
 
+class Pipe(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("imgs\pipe.png")
+
+
 bird_group = pygame.sprite.Group()
 
 flappy = Alien(50, int(screen_height) / 2)
