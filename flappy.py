d9d51ecbab8aaefb5519cd603939e87a0a18de79
SchoolNerdProgrammer <colemarshall12389@gmail.com>
2023-03-24 12:45:56 +0000

    From PC update- fixed error

--- flappy.py
+++ flappy.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+
 pygame.init()
 
 clock = pygame.time.Clock()
@@ -8,36 +9,33 @@ fps = 60
 screen_width = 432
 screen_height = 468
 
-screen = pygame.display.set_mode((screen_width,screen_height))
+screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Flappy Alien (Earthquake DLC)")
 
-
-#game variables
+# game variables
 ground_scroll = 0
 scroll_speed = 1.5
 
-
-
-#load images
+# load images
 bg = pygame.image.load("space background.png")
 ground_img = pygame.image.load("cartoon ground.png")
-ground_img = pygame.transform.scale(ground_img,(1500,468))
+ground_img = pygame.transform.scale(ground_img, (1500, 468))
 
 
-#alien class
+# alien class
 class Alien(pygame.sprite.Sprite):
     def __init__(self, x, y):
-        pygame.sprite.Sprite.__init__(self) 
+        pygame.sprite.Sprite.__init__(self)
         self.images = []
         self.index = 0
         self.counter = 0
-        for num in range(1,4):
-            img = pygame.image.load(f"alien thing{num}.png")##working - need to create an alien to flap the wings of.
-            img = pygame.transform.scale(img,(64,64))
+        for num in range(1, 4):
+            img = pygame.image.load(f"alien thing{num}.png")  # working - need to create an alien to flap the wings of.
+            img = pygame.transform.scale(img, (64, 64))
             self.images.append(img)
-        self.image = self.images[self.index
+        self.image = self.images[self.index]
         self.rect = self.image.get_rect()
-        self.rect.center = [x,y]
+        self.rect.center = [x, y]
         self.vel = 0
 
     def update(self):
@@ -49,15 +47,9 @@ class Alien(pygame.sprite.Sprite):
         if self.rect.bottom < 390:
             self.rect.y += int(self.vel)
 
-        #jump
+        # jump
         if pygame.mouse.get_pressed()[0] == 1:
             self.vel = -10
-
-
-
-
-
-
 
         if self.counter > flap_cooldown:
             self.counter = 0
@@ -65,40 +57,39 @@ class Alien(pygame.sprite.Sprite):
             if self.index == 3:
                 self.index = 0
         self.image = self.images[self.index]
+
+
 bird_group = pygame.sprite.Group()
 
-flappy = Alien(50,int(screen_height)/ 2)
+flappy = Alien(50, int(screen_height) / 2)
 
 bird_group.add(flappy)
 
-
 bird_group.draw(screen)
 bird_group.update()
-
 
 run = True
 while run:
 
     clock.tick(fps)
 
+    # background image drawn
+    screen.blit(bg, (0, 0))
 
-    #background image drawn
-    screen.blit(bg, (0,0))
-    
-    #draw the alien bird thing
+    # draw the alien bird thing
     bird_group.draw(screen)
     bird_group.update()
 
-    #draw and scroll ground
-    screen.blit(ground_img, (ground_scroll,180))
+    # draw and scroll ground
+    screen.blit(ground_img, (ground_scroll, 180))
     ground_scroll -= scroll_speed
     if abs(ground_scroll) > 1000:
         ground_scroll = 0
 
-    #event of quitting the game window
+    # event of quitting the game window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
-    #update the game window
+    # update the game window
     pygame.display.update()
