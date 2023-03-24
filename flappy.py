9c405ed51da590ec114ead9948f31654ca149fed
SchoolNerdProgrammer <colemarshall12389@gmail.com>
2023-03-24 12:57:46 +0000

    From PC update- fixed error

--- flappy.py
+++ flappy.py
@@ -17,8 +17,8 @@ ground_scroll = 0
 scroll_speed = 1.5
 
 # load images
-bg = pygame.image.load("space background.png")
-ground_img = pygame.image.load("cartoon ground.png")
+bg = pygame.image.load("imgs\space background.png")
+ground_img = pygame.image.load("imgs\cartoon ground.png")
 ground_img = pygame.transform.scale(ground_img, (1500, 468))
 
 
@@ -30,7 +30,7 @@ class Alien(pygame.sprite.Sprite):
         self.index = 0
         self.counter = 0
         for num in range(1, 4):
-            img = pygame.image.load(f"alien thing{num}.png")  # working - need to create an alien to flap the wings of.
+            img = pygame.image.load(f"imgs\ alien thing{num}.png")
             img = pygame.transform.scale(img, (64, 64))
             self.images.append(img)
         self.image = self.images[self.index]
